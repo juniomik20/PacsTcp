8e749167fd6fe8c47234e80c78fd86a3c98c7c39
Mike <mj.egea@sarria.salesians.cat>
2019-02-18 15:40:45 +0100

    inital commit

---
+++ FuncionSerClie/SendTcp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace FuncionSerClie
+{
+    public class SendTcp
+    {
+        Byte[] data = new Byte[1024];
+
+
+        public void sendMessage(string mensajeOrPath,string ip, int port) {
+
+            TcpClient client = new TcpClient(ip, port);
+            NetworkStream stream = client.GetStream();
+            data = System.Text.Encoding.ASCII.GetBytes(mensajeOrPath);
+            stream.Write(data, 0, data.Length);
+
+        }
+
+
+
+    }
+}
